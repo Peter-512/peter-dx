d807f59208329cc0bda0e82ebed3fcda3827736e
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-09-09 06:35:52 +0200

    optimized images and added logos

--- src/lib/images/index.ts
+++ src/lib/images/index.ts
@@ -1,43 +1,27 @@
 /* eslint-disable */
 
-import bouldering1Src from './bouldering-1.jpeg';
-import bouldering2Src from './bouldering-2.jpeg';
-import bouldering3Src from './bouldering-3.jpeg';
-import bowlingSrc from './bowling.jpeg';
+import boulderingSrc from './bouldering.jpeg';
 import climbing1Src from './climbing-1.jpeg';
 import climbing2Src from './climbing-2.jpeg';
 import climbing3Src from './climbing-3.jpeg';
-import climbing4Src from './climbing-4.jpeg';
-import climbing5Src from './climbing-5.jpeg';
-import climbing6Src from './climbing-6.jpeg';
 import expo1Src from './expo-1.jpeg';
 import expo2Src from './expo-2.jpeg';
 import expo3Src from './expo-3.jpeg';
 import expo4Src from './expo-4.jpg';
-import familySrc from './family.jpg';
 import selfieSrc from './selfie.jpeg';
-import sisterSrc from './sister.jpeg';
 import styleSrc from './style.jpeg';
 import subwaySrc from './subway.jpeg';
 import weirdStareSrc from './weird-stare.jpeg';
 
-const bouldering1 = { src: bouldering1Src, alt: 'Bouldering' };
-const bouldering2 = { src: bouldering2Src, alt: 'Bouldering' };
-const bouldering3 = { src: bouldering3Src, alt: 'Bouldering' };
-const bowling = { src: bowlingSrc, alt: 'Bowling' };
+const bouldering = { src: boulderingSrc, alt: 'Bouldering' };
 const climbing1 = { src: climbing1Src, alt: 'Climbing' };
 const climbing2 = { src: climbing2Src, alt: 'Climbing' };
 const climbing3 = { src: climbing3Src, alt: 'Climbing' };
-const climbing4 = { src: climbing4Src, alt: 'Climbing' };
-const climbing5 = { src: climbing5Src, alt: 'Climbing' };
-const climbing6 = { src: climbing6Src, alt: 'Climbing' };
 const expo1 = { src: expo1Src, alt: 'Expo' };
 const expo2 = { src: expo2Src, alt: 'Expo' };
 const expo3 = { src: expo3Src, alt: 'Expo' };
 const expo4 = { src: expo4Src, alt: 'Expo' };
-const family = { src: familySrc, alt: 'Family' };
 const selfie = { src: selfieSrc, alt: 'Selfie' };
-const sister = { src: sisterSrc, alt: 'Sister' };
 const style = { src: styleSrc, alt: 'Style' };
 const subway = { src: subwaySrc, alt: 'Subway' };
 const weirdStare = { src: weirdStareSrc, alt: 'Weird Stare' };
@@ -47,19 +31,11 @@ export {
 	expo2,
 	expo3,
 	expo4,
-	bouldering1,
-	bouldering2,
-	bouldering3,
-	bowling,
+	bouldering,
 	climbing1,
 	climbing2,
 	climbing3,
-	climbing4,
-	climbing5,
-	climbing6,
-	family,
 	selfie,
-	sister,
 	style,
 	subway,
 	weirdStare
---
+++ src/lib/logos/index.ts
@@ -0,0 +1,7 @@
+import datacampLogo from './datacamp.webp';
+import shadcnSvelteLogo from './shadcn-svelte.png';
+import supabaseLogo from './supabase.png';
+import svelteLogo from './svelte.png';
+import vercelLogo from './vercel.png';
+
+export { datacampLogo, shadcnSvelteLogo, supabaseLogo, svelteLogo, vercelLogo };
