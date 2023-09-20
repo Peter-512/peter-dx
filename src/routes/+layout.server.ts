c85dd0ac403b91d3f64376735835b7fd936f8562
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-09-20 07:22:54 +0200

    ordered testimonials decending based on date

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -5,7 +5,8 @@ import type { LayoutServerLoad } from './$types';
 export const load: LayoutServerLoad = async () => {
 	const { data, error: e } = await supabase
 		.from('testimonials')
-		.select('name, description, quote, slug, image_url');
+		.select('name, description, quote, slug, image_url')
+		.order('received_at', { ascending: false });
 	if (e) throw error(404, 'Not found');
 	return { testimonials: data };
 };
