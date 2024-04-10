94ca9595ba084acba12539be83b3caba0893ce8c
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2024-04-10 21:38:19 +0200

    dumb fix

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -1,9 +1,12 @@
+import { dev } from '$app/environment';
 import type { BlogPost } from '$lib/types/types';
 
 export const prerender = true;
 
+const url = dev ? 'http://localhost:5173' : 'https://peter-dx.vercel.app';
+
 export async function GET({ fetch }) {
-	const response = await fetch('api/posts');
+	const response = await fetch('api/blog');
 	const posts: BlogPost[] = await response.json();
 
 	const headers = { 'Content-Type': 'application/xml' };
@@ -13,16 +16,16 @@ export async function GET({ fetch }) {
 			<channel>
 				<title>Peter Buschenreiter's blog</title>
 				<description>Just writing about things that excite me</description>
-				<link>https://peter-dx.vercel.app/</link>
-				<atom:link href='https://peter-dx.vercel.app/rss.xml' rel='self' type='application/rss+xml'/>
+				<link>${url}</link>
+				<atom:link href='${url}/rss.xml' rel='self' type='application/rss+xml'/>
 				${posts
 					.map(
 						(post) => `
 						<item>
 							<title>${post.title}</title>
 							<description>${post.description}</description>
-							<link>https://peter-dx.vercel.app/blog/${post.slug}</link>
-							<guid isPermaLink='true'>https://peter-dx.vercel.app/blog/${post.slug}</guid>
+							<link>${url}/blog/${post.slug}</link>
+							<guid isPermaLink='true'>${url}/blog/${post.slug}</guid>
 							<pubDate>${new Date(post.date).toUTCString()}</pubDate>
 						</item>
 					`
