fc36cb482ae8dc707d13a6124119cc4694a9b151
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2024-04-13 11:11:23 +0200

    better focus styles

--- src/routes/blog/TagBadge.svelte
+++ src/routes/blog/TagBadge.svelte
@@ -12,7 +12,7 @@
 </script>
 
 <button
-	class="py-1 no-underline px-3 rounded-full bg-slate-800 hover:bg-slate-700 whitespace-nowrap {size === 'sm' ? 'text-xs' : 'text-sm'}"
+	class="py-1 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring no-underline px-3 rounded-full bg-slate-800 hover:bg-slate-700 whitespace-nowrap {size === 'sm' ? 'text-xs' : 'text-sm'}"
 	on:click={onClick}
 	transition:fly
 	use:transition={`tag-badge-${tag}-${transitionKey}-1`}>&num;{tag}
