b5811449c3057d506aa07b994cc24b073b72d614
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2024-04-14 10:51:55 +0200

    converted Button

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -1,24 +1,30 @@
-<script lang="ts">
-	import { Button as ButtonPrimitive } from "bits-ui";
-	import { cn } from "$lib/utils";
-	import { buttonVariants, type Props, type Events } from ".";
+<script lang='ts'>
+	import { Button as ButtonPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils';
+	import { buttonVariants, type Props, type Events } from '.';
 
 	type $$Props = Props;
 	type $$Events = Events;
 
-	let className: $$Props["class"] = undefined;
-	export let variant: $$Props["variant"] = "default";
-	export let size: $$Props["size"] = "default";
-	export let builders: $$Props["builders"] = [];
-	export { className as class };
+	let {
+		variant = 'default',
+		size = 'default',
+		builders = [],
+		class: className = undefined,
+		...restProps
+	}: {
+		variant?: $$Props['variant'];
+		size?: $$Props['size'];
+		builders?: $$Props['builders'];
+		class?: $$Props['class'];
+	} = $props();
+
 </script>
 
 <ButtonPrimitive.Root
+	{...restProps}
 	{builders}
 	class={cn(buttonVariants({ variant, size, className }))}
-	{...$$restProps}
-	on:click
-	on:keydown
 >
 	<slot />
 </ButtonPrimitive.Root>
